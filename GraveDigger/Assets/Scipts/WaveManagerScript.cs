3891561be8b3d9d87337e27ddd0ed2b1c677a96d
a455c <isaacupcroft@gmail.com>
2024-10-30 15:30:15 +0000

    remade Wavemanager script but needs fixing

--- GraveDigger/Assets/Scipts/WaveManagerScript.cs
+++ GraveDigger/Assets/Scipts/WaveManagerScript.cs
@@ -1,83 +1,188 @@
+using JetBrains.Annotations;
+using Pathfinding.Serialization;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Data;
+using UnityEngine.InputSystem.Interactions;
 
 public class WaveManagerScript : MonoBehaviour
 {
+	/*
+		[SerializeField] GameObject zombiePrefab;
+		[SerializeField] GameObject zombieParent;
+		[SerializeField] GameObject zombieSpawn;
 
-	[SerializeField] GameObject zombiePrefab;
-	[SerializeField] GameObject zombieParent;
-	[SerializeField] GameObject zombieSpawn;
+		[SerializeField] TMP_Text waveText;
 
-	[SerializeField] TMP_Text waveText;
+		int currentZombies = 0;
+		int currentWave = 0;
+		int newZombies = 5;
 
-	int currentZombies = 0;
-	int currentWave = 0;
-	int newZombies = 5;
+		int spawnradius = 1;
 
-	int spawnradius = 1;
-
-	bool waveEnded = true;
+		bool waveEnded = true;
 
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		waveEnded = true;
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-		currentZombies = zombieParent.transform.childCount;
-		if (waveEnded)
+		// Start is called before the first frame update
+		void Start()
 		{
-
-			waveEnded = false;
-			currentWave += 1;
-			waveText.text = "Wave " + currentWave.ToString();
-
-			newZombies *= 2;
-
-			print(newZombies);
-			spawnError();
-			for (int i = 0; i < newZombies; i++)
-			{
-				StartCoroutine(spawn());
-			}
-
-			currentZombies = newZombies;	
-		}
-
-		if (currentZombies == 0 && !waveEnded)
-		{
-			// wait intermission time
 			waveEnded = true;
 		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			currentZombies = zombieParent.transform.childCount;
+			if (waveEnded)
+			{
+
+				waveEnded = false;
+				currentWave += 1;
+				waveText.text = "Wave " + currentWave.ToString();
+
+				newZombies *= 2;
+
+				print(newZombies);
+				spawnError();
+				for (int i = 0; i < newZombies; i++)
+				{
+					StartCoroutine(spawn());
+				}
+
+				currentZombies = newZombies;	
+			}
+
+			if (currentZombies == 0 && !waveEnded)
+			{
+				// wait intermission time
+				waveEnded = true;
+			}
+		}
+
+		IEnumerator spawn()
+		{
+			yield return new WaitForSeconds(Random.Range(0.05f, 2f));
+
+			GameObject clone = Instantiate(zombiePrefab);
+			clone.transform.SetParent(zombieParent.transform, false);
+
+			int x = Random.Range(-spawnradius, spawnradius);
+			int y = Random.Range(-spawnradius, spawnradius);
+			Vector3 spawnpoint = new Vector3(zombieSpawn.transform.position.x + x, zombieSpawn.transform.position.y + y, zombieSpawn.transform.position.z);
+			clone.transform.position = spawnpoint;
+		}
+
+		void spawnError()
+		{
+			GameObject clone = Instantiate(zombiePrefab);
+			clone.transform.SetParent(zombieParent.transform, false);
+
+			int x = Random.Range(-spawnradius, spawnradius);
+			int y = Random.Range(-spawnradius, spawnradius);
+			Vector3 spawnpoint = new Vector3(zombieSpawn.transform.position.x + x, zombieSpawn.transform.position.y + y, zombieSpawn.transform.position.z);
+			clone.transform.position = spawnpoint;
+		}*/
+
+	public GameObject _zombiePrefab;
+	public GameObject _gravePrefab;
+	public GameObject _zombieHolder;
+	public TransformListHolder _gravesList;
+	public TMP_Text _waveText;
+
+
+	public int newZombies = 2;
+	public int currentZombies = 2;
+	public int newGraves = 2;
+
+	public int currentWave = 0;
+
+	public bool waveStarted = false;
+	public bool gravesDestroyed = true;
+
+	float intermissionTime = 10;
+
+	float pTime;
+	int spawnradius = 2;
+
+	private void Start()
+	{
+		pTime = Time.time;
 	}
 
-	IEnumerator spawn()
+	private void Update()
 	{
-		yield return new WaitForSeconds(Random.Range(0.05f, 2f));
+		currentZombies = _zombieHolder.transform.childCount;
+		if (_gravesList.Variable.Count == 0)
+		{
+			gravesDestroyed = true;
+		}
+		if(currentZombies == 0 && waveStarted)
+		{
+			print("current zombies 0");
+			waveStarted = false;
+			pTime = Time.time;
+			currentWave += 1;
+			_waveText.text = currentWave.ToString();
+			newZombies = newZombies + 4 + currentWave * 2;
+			newGraves = currentWave;
+		}
 
-		GameObject clone = Instantiate(zombiePrefab);
-		clone.transform.SetParent(zombieParent.transform, false);
+		if(!waveStarted && gravesDestroyed)
+		{
+			print("spawninggraves");
+			_waveText.text = Mathf.RoundToInt(0 - (pTime - Time.time)).ToString();
+			if (Mathf.RoundToInt(Time.time - pTime) == intermissionTime)
+			{
+				print("entered if statement");
+				gravesDestroyed = false;
+				Spawn_Graves();
+			}
+		}
+		else if(!waveStarted && !gravesDestroyed)
+		{
+			//same number of zombies spawn at all graves
+			_waveText.text = currentWave.ToString();
+			waveStarted = true;
+
+			StartCoroutine(Spawn_Zombies(0, _gravesList.Variable[0].transform));
+			for (int z = 0; z < _gravesList.Variable.Count - 1; z++)
+			{
+				for (int i = 0; i < newZombies - 1; i++)
+				{
+					float t = Random.Range(0.05f, 2f);
+					Transform graveToSpawnAt = _gravesList.Variable[z].transform;
+					Spawn_Zombies(t, graveToSpawnAt);
+				}
+			}
+		}
+	}
+
+	IEnumerator Spawn_Zombies(float t, Transform graveToSpawnAt)
+	{
+		yield return new WaitForSeconds(t);
+
+		GameObject clone = Instantiate(_zombiePrefab);
+		clone.transform.SetParent(_zombieHolder.transform, false);
 
 		int x = Random.Range(-spawnradius, spawnradius);
 		int y = Random.Range(-spawnradius, spawnradius);
-		Vector3 spawnpoint = new Vector3(zombieSpawn.transform.position.x + x, zombieSpawn.transform.position.y + y, zombieSpawn.transform.position.z);
+		Vector3 spawnpoint = new Vector3(graveToSpawnAt.position.x + x, graveToSpawnAt.position.y + y, graveToSpawnAt.position.z);
 		clone.transform.position = spawnpoint;
 	}
 
-	void spawnError()
+	void Spawn_Graves()
 	{
-		GameObject clone = Instantiate(zombiePrefab);
-		clone.transform.SetParent(zombieParent.transform, false);
+		for (int i = 0; i < newGraves; i++)
+		{
+			// find random location and place there
+			int x = Random.Range(-20, 20);
+			int y = Random.Range(-20, 20);
 
-		int x = Random.Range(-spawnradius, spawnradius);
-		int y = Random.Range(-spawnradius, spawnradius);
-		Vector3 spawnpoint = new Vector3(zombieSpawn.transform.position.x + x, zombieSpawn.transform.position.y + y, zombieSpawn.transform.position.z);
-		clone.transform.position = spawnpoint;
+			GameObject clone = Instantiate(_gravePrefab);
+			clone.transform.position = new Vector3(x, y, 0);
+		}
 	}
+
 }
